1393d64ebc854383c6ef5db381e872949c157312
Eduardo Zawitoski <71409853+Zawitoski@users.noreply.github.com>
2025-10-21 21:03:50 +0000

    Alteração da Home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <div style={{ textAlign: 'center', marginTop: '100px' }}>
+            <h1>Quem leu mam... Brincadeira!😂 </h1>
+            <h1>Bom curso companheiro! 🫡 </h1>
+        </div>
+    );
 }
 
 export default Home;
