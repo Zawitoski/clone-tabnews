be3952665b1f896295ca00754404667da01e9199
Eduardo Zawitoski <71409853+Zawitoski@users.noreply.github.com>
2025-11-04 21:04:38 +0000

    adiciona arquivos `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório do Curso.dev do Filipe Deschamps. Implementação do https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home() {
-    return (
-        <div style={{ textAlign: 'center', marginTop: '100px' }}>
-            <h1>Quem leu mam... Brincadeira!😂 </h1>
-            <h1>Bom curso companheiro! 🫡 </h1>
-        </div>
-    );
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Quem leu mam... Brincadeira!😂 </h1>
+      <h1>Bom curso companheiro! 🫡 </h1>
+    </div>
+  );
 }
 
 export default Home;
